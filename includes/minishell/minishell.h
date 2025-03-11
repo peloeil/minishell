a0314d9a9870a53be172218942a7edae5f8f3423
Shun Ota <peloeil20@gmail.com>
2025-03-11 17:59:01 +0900

    fix: norm

--- includes/minishell/minishell.h
+++ includes/minishell/minishell.h
@@ -1,8 +1,20 @@
-#ifndef MNISHELL_H
-# define MNISHELL_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sota <sota@student.42tokyo.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 17:56:03 by sota              #+#    #+#             */
+/*   Updated: 2025/03/11 17:56:22 by sota             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 # define PROMPT "minishell$ "
 
 char	*wrap_readline(const char *prompt, const int is_interactive);
 
-#endif // MNISHELL_H
+#endif // MINISHELL_H
--- srcs/main.c
+++ srcs/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sota <sota@student.42tokyo.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 17:49:27 by sota              #+#    #+#             */
+/*   Updated: 2025/03/11 17:58:03 by sota             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <minishell/minishell.h>
 #include <stdlib.h>
 #include <libft/ft_stdio.h>
@@ -8,7 +20,9 @@ int	main(void)
 
 	while (1)
 	{
-		input = wrap_readline(PROMPT, isatty(STDIN_FILENO) && isatty(STDERR_FILENO));
+		input = wrap_readline(
+				PROMPT, isatty(STDIN_FILENO) && isatty(STDERR_FILENO)
+				);
 		if (input == NULL)
 			break ;
 		ft_printf("%s\n", input);
