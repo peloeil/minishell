05f27a9e934cf71315a5d2343388981c72a031a6
yukito-onuma <onuma.y.473@ms.saitama-u.ac.jp>
2025-04-12 17:58:09 +0900

    env

---
+++ srcs/builtin/env.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonuma <yonuma@student.42.fr>              #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-04-12 08:35:57 by yonuma            #+#    #+#             */
+/*   Updated: 2025-04-12 08:35:57 by yonuma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell/minishell.h>
+#include <stdlib.h>
+#include <libft/ft_stdio.h>
+
+int env(char **argv, char **envp)
+{
+    int	i;
+
+    (void)argv;
+    if (envp == NULL)
+        return (EXIT_FAILURE);
+    i = 0;
+    while (envp[i] != NULL)
+    {
+        ft_printf("%s\n", envp[i]);
+        i++;
+    }
+    return (EXIT_SUCCESS);
+}
