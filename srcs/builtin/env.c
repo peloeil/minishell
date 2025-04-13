a5d67835c9c39bfa1a8e9fcd61712d78cd01ca61
yukito-onuma <onuma.y.473@ms.saitama-u.ac.jp>
2025-04-13 16:02:59 +0900

    builtn env

--- srcs/builtin/env.c
+++ srcs/builtin/env.c
@@ -24,8 +24,8 @@ int env(char **argv, const char **envp)
     i = 0;
     while (envp[i] != NULL)
     {
-        ft_printf("%s\n", envp[i]);
-        i++;
+    ft_printf("%s\n", envp[i]);
+    i++;
     }
     return (EXIT_SUCCESS);
 }
