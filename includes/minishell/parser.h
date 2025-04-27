c437a14788cc45d5bba8406d8e416585a1a3f243
Shun Ota <peloeil20@gmail.com>
2025-04-27 21:01:47 +0900

    chore: add functinons to search token

--- includes/minishell/parser.h
+++ includes/minishell/parser.h
@@ -10,6 +10,9 @@ typedef struct s_ast_node
 	struct s_ast_node	*right;
 }	t_ast_node;
 
+t_list		*search_token_from_left(t_token_id id, t_list *start, t_list *end);
+t_list		*search_token_from_right(t_token_id id, t_list *start, t_list *end);
 t_ast_node	*parse_tokens(t_list *start, t_list *end);
+t_ast_node	*parse_command(t_list *start, t_list *end);
 
 #endif // PARSER_H
