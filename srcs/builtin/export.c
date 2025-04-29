4da684c8ad58d231cb33854211fb4ea014b7992e
Shun Ota <peloeil20@gmail.com>
2025-04-30 00:23:04 +0900

    fix: space to tab

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 09:39:15 by yonuma            #+#    #+#             */
-/*   Updated: 2025/04/26 18:37:45 by marvin           ###   ########.fr       */
+/*   Updated: 2025/04/30 00:25:13 by sota             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,40 @@
 #include <libft/ft_string.h>
 #include <string.h>
 
-void sort_envp(t_minishell_envp **head)
+void	sort_envp(t_minishell_envp **head)
 {
-    int 				sorted;
-    t_minishell_envp 	**curr;
+	int					sorted;
+	t_minishell_envp	**curr;
 
-    sorted = 0;
-    while (!sorted)
-    {
-        sorted = 1;
-        curr = head;
-        while ((*curr) && (*curr)->next)
-        {
-            if (ft_strcmp((*curr)->key, (*curr)->next->key) > 0)
-            {
-                t_minishell_envp *tmp = (*curr)->next;
-                (*curr)->next = tmp->next;
-                tmp->next = *curr;
-                *curr = tmp;
-                sorted = 0;
-            }
-            curr = &(*curr)->next;
-        }
-    }
+	sorted = 0;
+	while (!sorted)
+	{
+		sorted = 1;
+		curr = head;
+		while ((*curr) && (*curr)->next)
+		{
+			if (ft_strcmp((*curr)->key, (*curr)->next->key) > 0)
+			{
+				t_minishell_envp *tmp = (*curr)->next;
+				(*curr)->next = tmp->next;
+				tmp->next = *curr;
+				*curr = tmp;
+				sorted = 0;
+			}
+			curr = &(*curr)->next;
+		}
+	}
 }
 
-void print_sorted_env(int fd, t_minishell_envp *envp)
+void	print_sorted_env(int fd, t_minishell_envp *envp)
 {
-    sort_envp(&envp);
-    while (envp != NULL)
-    {
-        if (envp->key[0] != '_')
-            ft_dprintf(fd, "declare -x %s=\"%s\"\n", envp->key, envp->value);
-        envp = envp->next;
-    }
+	sort_envp(&envp);
+	while (envp != NULL)
+	{
+		if (envp->key[0] != '_')
+			ft_dprintf(fd, "declare -x %s=\"%s\"\n", envp->key, envp->value);
+		envp = envp->next;
+	}
 }
 
 void	register_env_with_value(t_minishell_envp *envp, char *key, char *value)
@@ -124,11 +124,11 @@ void	register_env(t_minishell_envp *envp, char *str)
 	register_env_with_value(envp, key, value);
 }
 
-int export(int fd, char *argv[], t_minishell_envp *envp)
+int	export(int fd, char *argv[], t_minishell_envp *envp)
 {
-    if (argv[1] == NULL)
-        print_sorted_env(fd, envp);
-    else
-        register_env(envp, argv[1]);
-    return (0);
+	if (argv[1] == NULL)
+		print_sorted_env(fd, envp);
+	else
+		register_env(envp, argv[1]);
+	return (0);
 }
