36787a673e5923c2a0926cfcd368e480a38b3e45
Shun Ota <peloeil20@gmail.com>
2025-05-02 17:44:16 +0900

    fix: test output redirection

--- integration_tests/test.sh
+++ integration_tests/test.sh
@@ -24,9 +24,6 @@ test_bash() {
     echo "$command" | ./minishell >/tmp/out_minishell 2>/tmp/err_minishell &
     local minishell_pid=$!
 
-	mv out_minishell /tmp/out_minishell
-	mv err_minishell /tmp/err_minishell
-
     # Wait for both processes to complete
     wait $bash_pid
     bash_status=$?
