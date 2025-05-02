8a1f3397d9737ff1ffa0c45dd58626855aa9665c
Shun Ota <peloeil20@gmail.com>
2025-05-02 17:07:45 +0900

    fix: norm

--- srcs/builtin/utils.c
+++ srcs/builtin/utils.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 09:03:16 by marvin            #+#    #+#             */
-/*   Updated: 2025/05/02 22:49:48 by marvin           ###   ########.fr       */
+/*   Updated: 2025/05/02 23:05:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,26 +44,32 @@ void	print_sorted_env(int fd, t_envp *envp)
 
 void	sort_envp(t_envp **head)
 {
-	int		sorted;
-	t_envp	**curr;
-	t_envp	*tmp;
+    int		sorted;
+    t_envp	*curr;
+	char	*tmp_key;
+	char	*tmp_value;
 
-	sorted = 0;
-	while (!sorted)
-	{
-		sorted = 1;
-		curr = head;
-		while ((*curr) && (*curr)->next)
-		{
-			if (ft_strcmp((*curr)->key, (*curr)->next->key) > 0)
-			{
-				tmp = (*curr)->next;
-				(*curr)->next = tmp->next;
-				tmp->next = *curr;
-				*curr = tmp;
-				sorted = 0;
-			}
-			curr = &(*curr)->next;
-		}
-	}
+    sorted = 0;
+    while (!sorted)
+    {
+        sorted = 1;
+        curr = *head;
+        while (curr && curr->next)
+        {
+            if (ft_strcmp(curr->key, curr->next->key) > 0)
+            {
+                tmp_key = curr->key;
+                tmp_value = curr->value;
+
+                curr->key = curr->next->key;
+                curr->value = curr->next->value;
+
+                curr->next->key = tmp_key;
+                curr->next->value = tmp_value;
+
+                sorted = 0;
+            }
+            curr = curr->next;
+        }
+    }
 }
