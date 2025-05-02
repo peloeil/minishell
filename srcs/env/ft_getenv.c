bf67c087110c6fa98e8d58673cd7eb122cb9a2a4
Shun Ota <peloeil20@gmail.com>
2025-05-03 01:10:52 +0900

    fix: ft_getenv to use t_envp

--- srcs/env/ft_getenv.c
+++ srcs/env/ft_getenv.c
@@ -6,26 +6,26 @@
 /*   By: sota <sota@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 22:18:58 by sota              #+#    #+#             */
-/*   Updated: 2025/04/10 13:55:52 by sota             ###   ########.fr       */
+/*   Updated: 2025/05/03 00:55:12 by sota             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <minishell/minishell.h>
 #include <libft/ft_string.h>
 
-char	*ft_getenv(const char *key, const char **envp)
+char	*ft_getenv(const char *key, const t_envp *envp)
 {
-	size_t	i;
-	size_t	key_len;
 	char	*value;
 
-	key_len = ft_strlen(key);
-	i = 0;
 	value = NULL;
-	while (value == NULL && envp[i] != NULL)
+	while (envp != NULL)
 	{
-		if (ft_strncmp(key, envp[i], key_len) == 0 && envp[i][key_len] == '=')
-			value = (char *)envp[i] + key_len + 1;
-		i++;
+		if (ft_strcmp(key, envp->key) == 0)
+		{
+			value = envp->value;
+			break ;
+		}
+		envp = envp->next;
 	}
 	return (value);
 }
--- unit_tests/env/ft_getenv.c
+++ unit_tests/env/ft_getenv.c
@@ -1,32 +1,41 @@
+#include <minishell/minishell.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <minishell/minishell.h>
 
-static void test(const char *key, const char **envp) {
-	char *expected = getenv(key);
-	char *actual = ft_getenv(key, envp);
+static void test(const char *key, const t_envp *envp) {
+    char *expected = getenv(key);
+    char *actual = ft_getenv(key, envp);
 
-	if (expected == NULL && actual != NULL) {
-		exit(1);
-	}
-	if (expected != NULL && actual == NULL) {
-		exit(1);
-	}
-	if (expected == NULL && actual == NULL) {
-		return;
-	}
-	if (strcmp(expected, actual) != 0) {
-		exit(1);
-	}
+    if (expected == NULL && actual != NULL) {
+        printf("Error: expected NULL for key '%s', got '%s'\n", key, actual);
+        free_ms_envp((t_envp *)envp);
+        exit(1);
+    }
+    if (expected != NULL && actual == NULL) {
+        printf("Error: expected '%s' for key '%s', got NULL\n", expected, key);
+        free_ms_envp((t_envp *)envp);
+        exit(1);
+    }
+    if (expected == NULL && actual == NULL) { return; }
+    if (strcmp(expected, actual) != 0) {
+        printf("Error: expected '%s' for key '%s', got '%s'\n", expected, key, actual);
+        free_ms_envp((t_envp *)envp);
+        exit(1);
+    }
 }
 
 int main(int argc, char **argv, char **envp) {
-	(void)argc;
-	(void)argv;
-	test("PATH", (const char **)envp);
-	test("XDG_CONFIG_HOME", (const char **)envp);
-	test("USER", (const char **)envp);
-	test("KEY", (const char **)envp);
-	test("", (const char **)envp);
-	return 0;
+    (void)argc;
+    (void)argv;
+
+    t_envp *env = make_ms_envp(envp);
+
+    test("PATH", env);
+    test("XDG_CONFIG_HOME", env);
+    test("USER", env);
+    test("KEY", env);
+
+    free_ms_envp((t_envp *)env);
+    return 0;
 }
