96aa1a7c064bacd1aea5f6cc99244e43741e1a4b
Yukito-Onuma <onuma.y.473ms.saitama-u.ac.jp>
2025-05-03 20:45:28 +0900

    fix: コメント消した

--- srcs/builtin/env.c
+++ srcs/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 11:38:56 by marvin            #+#    #+#             */
-/*   Updated: 2025/05/03 11:55:28 by marvin           ###   ########.fr       */
+/*   Updated: 2025/05/03 20:45:06 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ int	env(int fd, t_envp *envp)
 			make_str(0, &str, envp->key, envp->value);
 		envp = envp->next;
 	}
-	// (void)fd;
 	ft_dprintf(fd, "%s", str.str);
 	return (0);
 }
