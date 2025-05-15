bcbbec2ebc214eb8f7184f98fdab588b6e9edea3
Shun Ota <peloeil20@gmail.com>
2025-05-15 13:39:28 +0900

    chore: remove unused includes

--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,14 @@
 /*   By: sota <sota@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 17:49:27 by sota              #+#    #+#             */
-/*   Updated: 2025/05/14 21:36:12 by sota             ###   ########.fr       */
+/*   Updated: 2025/05/15 04:52:57 by sota             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell/minishell.h>
 #include <minishell/execute.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <libft/ft_stdio.h>
-#include <libft/ft_string.h>
 #include <libft/ft_stdlib.h>
+#include <stdlib.h>
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -27,7 +24,7 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	if (make_ms_envp(&ms_envp, envp) == -1)
-		return (EXIT_FAILURE);
+		return (STATUS_ERRORS);
 	while (1)
 	{
 		cmd_str = wrap_readline(PROMPT);
