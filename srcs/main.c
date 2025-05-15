66e41bc48857f8a8294e613c17e25e9115b3555e
Shun Ota <peloeil20@gmail.com>
2025-05-15 22:36:18 +0900

    feat: have a working history

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,16 @@
 /*   By: sota <sota@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 17:49:27 by sota              #+#    #+#             */
-/*   Updated: 2025/05/15 04:52:57 by sota             ###   ########.fr       */
+/*   Updated: 2025/05/15 17:31:38 by sota             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell/minishell.h>
 #include <minishell/execute.h>
 #include <libft/ft_stdlib.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <readline/history.h>
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -32,6 +34,7 @@ int	main(int argc, char **argv, char **envp)
 			break ;
 		if (eval_cmd(cmd_str, ms_envp) == -1)
 			update_exit_status(STATUS_ERRORS, ms_envp);
+		add_history(cmd_str);
 		free(cmd_str);
 	}
 	status = ft_atoi(ft_getenv("?", ms_envp));
