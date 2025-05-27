33769619189541f3c16bd6eaa8e0e853bca742b2
Shun Ota <peloeil20@gmail.com>
2025-05-27 14:20:00 +0900

    fix: pwd return value

--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -6,11 +6,12 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 05:26:30 by yonuma            #+#    #+#             */
-/*   Updated: 2025/05/15 03:55:30 by sota             ###   ########.fr       */
+/*   Updated: 2025/05/27 14:27:32 by sota             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell/minishell.h>
+#include <minishell/execute.h>
 #include <stdlib.h>
 #include <libft/ft_stdio.h>
 
@@ -18,9 +19,11 @@ int	pwd(int fd, char **argv, t_envp **envp)
 {
 	char	*cwd;
 	int		need_free;
+	int		status;
 
 	(void)argv;
 	(void)envp;
+	status = STATUS_SUCCESS;
 	need_free = 1;
 	cwd = getcwd(NULL, 0);
 	if (cwd == NULL)
@@ -28,8 +31,9 @@ int	pwd(int fd, char **argv, t_envp **envp)
 		cwd = ft_getenv("!PWD", *envp);
 		need_free = 0;
 	}
-	ft_dprintf(fd, "%s\n", cwd);
+	if (ft_dprintf(fd, "%s\n", cwd) < 0)
+		status = STATUS_ERRORS;
 	if (need_free)
 		free(cwd);
-	return (0);
+	return (status);
 }
