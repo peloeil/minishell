78a031b64a2c09b57e784bef4d2af802aae49f98
Shun Ota <peloeil20@gmail.com>
2025-06-03 22:39:05 +0900

    refactor: pwd: remove getcwd

--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -6,34 +6,22 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 05:26:30 by yonuma            #+#    #+#             */
-/*   Updated: 2025/05/27 14:27:32 by sota             ###   ########.fr       */
+/*   Updated: 2025/06/03 21:53:14 by sota             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell/minishell.h>
 #include <minishell/execute.h>
-#include <stdlib.h>
 #include <libft/ft_stdio.h>
 
 int	pwd(int fd, char **argv, t_envp **envp)
 {
 	char	*cwd;
-	int		need_free;
-	int		status;
 
 	(void)argv;
 	(void)envp;
-	status = STATUS_SUCCESS;
-	need_free = 1;
-	cwd = getcwd(NULL, 0);
-	if (cwd == NULL)
-	{
-		cwd = ft_getenv("!PWD", *envp);
-		need_free = 0;
-	}
+	cwd = ft_getenv("!PWD", *envp);
 	if (ft_dprintf(fd, "%s\n", cwd) < 0)
-		status = STATUS_ERRORS;
-	if (need_free)
-		free(cwd);
-	return (status);
+		return (STATUS_ERRORS);
+	return (STATUS_SUCCESS);
 }
