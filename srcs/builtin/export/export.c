b48b2a58993ab17e74d77b60e6512faea914b131
Shun Ota <peloeil20@gmail.com>
2025-06-05 00:51:59 +0900

    chore: value = NULL when no value in export rather than empty string

--- srcs/builtin/export/export.c
+++ srcs/builtin/export/export.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 09:39:15 by yonuma            #+#    #+#             */
-/*   Updated: 2025/05/30 20:20:30 by sota             ###   ########.fr       */
+/*   Updated: 2025/06/04 16:13:17 by sota             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,10 @@ static int	register_env(t_envp **envp, char *str)
 	char	*key;
 	char	*value;
 
-	flag = (FLAG_EXPORT | FLAG_ENV);
 	if (split_into_key_value(str, &key, &value) == -1)
 		return (-1);
-	if (value[0] == '\0')
+	flag = (FLAG_EXPORT | FLAG_ENV);
+	if (value == NULL)
 		flag = FLAG_EXPORT;
 	free(value);
 	if (!is_valid_key(key))
--- srcs/env/split_char.c
+++ srcs/env/split_char.c
@@ -6,7 +6,7 @@
 /*   By: sota <sota@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 02:00:19 by sota              #+#    #+#             */
-/*   Updated: 2025/05/29 02:00:50 by sota             ###   ########.fr       */
+/*   Updated: 2025/06/05 00:22:28 by sota             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,24 @@
 int	split_into_key_value(char *str, char **key, char **value)
 {
 	char	*equal;
+	size_t	key_len;
+	size_t	value_len;
 
 	equal = ft_strchr(str, '=');
-	if (equal == NULL)
+	key_len = ft_strlen(str);
+	value_len = 0;
+	if (equal == str + key_len - 1)
+		key_len--;
+	else if (equal != NULL)
 	{
-		*key = ft_strdup(str);
-		*value = ft_strdup("");
+		value_len = key_len - (equal - str) - 1;
+		key_len = equal - str;
 	}
-	else
-	{
-		*key = ft_strndup(str, equal - str);
-		*value = ft_strdup(equal + 1);
-	}
-	if (*key == NULL || *value == NULL)
+	*key = ft_strndup(str, key_len);
+	*value = NULL;
+	if (value_len > 0)
+		*value = ft_strndup(equal + 1, value_len);
+	if (*key == NULL || (value_len > 0 && *value == NULL))
 	{
 		free(*key);
 		free(*value);
