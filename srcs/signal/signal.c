657f0033eb009434204ac00706c17b752be84570
Shun Ota <sota@student.42tokyo.jp>
2025-06-27 16:07:26 +0900

    fix: signal handler in readline (ok)

--- srcs/signal/signal.c
+++ srcs/signal/signal.c
@@ -6,67 +6,41 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 12:05:48 by marvin            #+#    #+#             */
-/*   Updated: 2025/06/11 18:10:45 by marvin           ###   ########.fr       */
+/*   Updated: 2025/06/27 16:07:01 by sota             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <minishell/signal.h>
 #include <signal.h>
 #include <readline/readline.h>
 #include <stdio.h>
-#include <termios.h>
-#include <minishell/signal.h>
 #include <unistd.h>
 
 volatile sig_atomic_t	g_received_signal = 0;
-
-static void	set_echoctl(int enable)
-{
-    struct termios term;
-
-    if (tcgetattr(STDIN_FILENO, &term) == 0)
-    {
-        if (enable)
-            term.c_lflag |= ECHOCTL;
-        else
-            term.c_lflag &= ~ECHOCTL;
-        tcsetattr(STDIN_FILENO, TCSANOW, &term);
-    }
-}
 
 static void	sig_handler(int signo)
 {
     g_received_signal = signo;
     if (signo == SIGINT && RL_ISSTATE(RL_STATE_READCMD))
     {
-        set_echoctl(1);
         rl_replace_line("", 0);
-        write(STDOUT_FILENO, "\n", 1);
+	write(STDOUT_FILENO, "\n", 1);
         rl_on_new_line();
 	rl_redisplay();
     }
-    else if (signo == SIGQUIT && RL_ISSTATE(RL_STATE_READCMD))
-    {
-        set_echoctl(0);
-	rl_on_new_line();
-        rl_redisplay();
-    }
-    else if (signo == SIGINT)
-    {
-        set_echoctl(1);
-	rl_replace_line("", 0);
-    }
-    else if (signo == SIGQUIT)
-        set_echoctl(0);
 }
 
 void	setup_signal(void)
 {
-    struct sigaction	act;
+    struct sigaction	sigint;
+    struct sigaction	sigquit;
 
-    // set_echoctl(1);
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
-    act.sa_handler = sig_handler;
-    sigaction(SIGINT, &act, NULL);
-    sigaction(SIGQUIT, &act, NULL);
+    sigemptyset(&sigint.sa_mask);
+    sigint.sa_flags = 0;
+    sigint.sa_handler = sig_handler;
+    sigaction(SIGINT, &sigint, NULL);
+    sigemptyset(&sigquit.sa_mask);
+    sigquit.sa_flags = 0;
+    sigquit.sa_handler = SIG_IGN;
+    sigaction(SIGQUIT, &sigquit, NULL);
 }
